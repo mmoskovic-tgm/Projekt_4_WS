fd8a3b1e0108bc27e436fe190a83ca9a6b46431a
dstudera-tgm <dstudera@student.tgm.ac.at>
2019-11-22 12:26:26 +0100

    Materialize

--- uebersicht-gui.php
+++ uebersicht-gui.php
@@ -3,8 +3,25 @@
 <head>
 <meta charset="utf-8">
 <title>FamArch</title>
+	
+	<link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 
 <body>
+	<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+	
+	<script type="text/javascript" src="js/materialize.min.js"></script>
 </body>
 </html>
