6aa254ca07da9fa6d0f12f7be5e47bac32468cac
mmoskovic-tgm <mmoskovic@student.tgm.ac.at>
2019-12-17 20:54:32 +0100

    alls umlaute geändert

--- insert.sql
+++ insert.sql
@@ -23,40 +23,40 @@ INSERT lebensdaten(id,nachname,vorname,gebDatum,gebOrt,stammbaum,geschlecht) VAL
 
 
 
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (6,"Hübner","Anton","Hübner","männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (7,"Jantsch","Magdalena","Hübner","weiblich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (8,"Ullrich","Josef","Hübner","männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (9,"Schiller","Anna","Hübner","weiblich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (10,"Teufert","Franz Anton","Hübner","männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (11,"Werner","Theresia","Hübner","weiblich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (12,"Phillip","*fehlt*","Hübner","männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (13,"*fehlt*","*fehlt*","Hübner","weiblich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (14,"Schwab","Phillip","Hübner","männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (15,"Löwenstein","Eliabetha","Hübner","weiblich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (16,"Hönig","Michael","Hübner","männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (17,"Lang","Maria","Hübner","weiblich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (18,"Zinnober","Michael","Hübner","männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (19,"Lechner","Katharina","Hübner","weiblich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (20,"Marian","Johann","Hübner","männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (21,"Dürrenberger","Barbara","Hübner","weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (6,"H&#252bner","Anton","H&#252bner","m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (7,"Jantsch","Magdalena","H&#252bner","weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (8,"Ullrich","Josef","H&#252bner","m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (9,"Schiller","Anna","H&#252bner","weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (10,"Teufert","Franz Anton","H&#252bner","m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (11,"Werner","Theresia","H&#252bner","weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (12,"Phillip","*fehlt*","H&#252bner","m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (13,"*fehlt*","*fehlt*","H&#252bner","weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (14,"Schwab","Phillip","H&#252bner","m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (15,"Löwenstein","Eliabetha","H&#252bner","weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (16,"Hönig","Michael","H&#252bner","m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (17,"Lang","Maria","H&#252bner","weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (18,"Zinnober","Michael","H&#252bner","m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (19,"Lechner","Katharina","H&#252bner","weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (20,"Marian","Johann","H&#252bner","m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (21,"Dürrenberger","Barbara","H&#252bner","weiblich");
 
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (22,"Hübner","Ferdinand","Hübner",6,7,"männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (23,"Ullrich","Anna","Hübner",8,9,"weiblich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (24,"Teufert","Anton Ferdinand","Hübner",10,11,"männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (25,"Phillip","Maria Josefa","Hübner",12,13,"weiblich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (26,"Schwab","Heinrich Ludwig I","Hübner",14,15,"männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (27,"Hönig","Maria","Hübner",16,17,"weiblich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (28,"Zinnober","Klemens","Hübner",18,19,"männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (29,"Marian","Maria","Hübner",20,21,"weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (22,"H&#252bner","Ferdinand","H&#252bner",6,7,"m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (23,"Ullrich","Anna","H&#252bner",8,9,"weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (24,"Teufert","Anton Ferdinand","H&#252bner",10,11,"m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (25,"Phillip","Maria Josefa","H&#252bner",12,13,"weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (26,"Schwab","Heinrich Ludwig I","H&#252bner",14,15,"m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (27,"Hönig","Maria","H&#252bner",16,17,"weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (28,"Zinnober","Klemens","H&#252bner",18,19,"m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (29,"Marian","Maria","H&#252bner",20,21,"weiblich");
 
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (30,"Hübner","Karl","Hübner",22,23,"männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (31,"Teufert","Maria Josefa","Hübner",24,25,"weiblich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (32,"Schwab","Heinrich Ludwig II","Hübner",26,27,"männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (33,"Zinnober","Karoline","Hübner",28,29,"weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (30,"Hübner","Karl","H&#252bner",22,23,"m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (31,"Teufert","Maria Josefa","H&#252bner",24,25,"weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (32,"Schwab","Heinrich Ludwig II","H&#252bner",26,27,"m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (33,"Zinnober","Karoline","H&#252bner",28,29,"weiblich");
 
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (34,"Hübner","Leopold","Hübner",30,31,"männlich");
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (35,"Schwab","Johanna Maria","Hübner",32,33,"weiblich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (34,"H&#252bner","Leopold","H&#252bner",30,31,"m&#228nnlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (35,"Schwab","Johanna Maria","H&#252bner",32,33,"weiblich");
 
-INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (36,"Hübner","Heinz Ludwig","Hübner",34,35,"männlich");
+INSERT lebensdaten(id,nachname,vorname,stammbaum,vater,mutter,geschlecht) VALUES (36,"H&#252bner","Heinz Ludwig","H&#252bner",34,35,"m&#228nnlich");
 
 
