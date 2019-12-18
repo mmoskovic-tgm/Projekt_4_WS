e1cb920d63f4615aec1f4dabea52a1248d7cc3fa
Nathan Baranow <nathan.baranow@gmail.com>
2019-12-18 19:29:00 +0100

    GebDatum Format gefixt

--- insert.sql
+++ insert.sql
@@ -8,7 +8,7 @@
 		INSERT lebensdaten(id,nachname,vorname,geschlecht) VALUES (22,"Werner","Theresia","weiblich");
 
 		INSERT lebensdaten(id,nachname,vorname,geschlecht) VALUES (23,"Phillip","*fehlt*","m&#228nnlich");
-		/**INSERT lebensdaten(id,nachname,vorname,stammbaum,geschlecht) VALUES (24,"*fehlt*","*fehlt*","H&#252bner","weiblich"); */
+		INSERT lebensdaten(id,nachname,vorname,geschlecht) VALUES (24,"*fehlt*","*fehlt*","weiblich");
 
 		INSERT lebensdaten(id,nachname,vorname,geschlecht) VALUES (25,"Schwab","Phillip","m&#228nnlich");
 		INSERT lebensdaten(id,nachname,vorname,geschlecht) VALUES (26,"L&#246wenstein","Eliabetha","weiblich");
@@ -23,20 +23,13 @@
 		INSERT lebensdaten(id,nachname,vorname,geschlecht) VALUES (32,"D&#252rrenberger","Barbara","weiblich");
 
 
-
-
-
-
-
-
-
-		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,mutter,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (9,"&#252bner","Ferdinand","-","-","-","-","06.07.1837","Reichenberg","r&#246m. kath.", "-","Tuchmacher","-","-",17,18,"Ullrich","25.09.1859","Reichenberg","Carl","1903","Wien","-","-","-","-","m&#228nnlich","X");
+		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,mutter,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (9,"H&#252bner","Ferdinand","-","-","-","-","06.07.1837","Reichenberg","r&#246m. kath.", "-","Tuchmacher","-","-",17,18,"Ullrich","25.09.1859","Reichenberg","Carl","1903","Wien","-","-","-","-","m&#228nnlich","X");
 
 		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,mutter,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (10,"H&#252bner","Anna","Ullrich","-","-","-","18.1.1839","Reichenberg","r&#246m. kath.", "-","Gemischtwaren-verscheiss","-","-",19,20,"Schiller","25.09.1859","Reichenberg","u.a. Carl","25.01.1925","Reichenberg","-","-","Wien","-","weiblich","X");
 
 		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,mutter,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (11,"Teufert","Anton Ferdinand","-","-","-","Teu-AN","30.03.1825","Liebenthal Nr. 109","r&#246m. kath.", "-","Tr&#246dler","-","-",21,22,"Werner","-","-","Maria Josefa Teufert + zwei weitere T&#246chter","11.02.1878","Hernals","-","13.02.1878","Hernals Friedhof","-","m&#228nnlich","X");
 
-		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (12,"Teufert","Maria Josefa","Philipp","-","-","-","17.03.1827","Lichtenthal","r&#246m. kath.", "-","Privat","-","-",23,"-","20.02.1855","Wien","Maria Josefa Teufert","19.03.1899","Hernals","-","21.03.1899","Friedhof Hernals","-","weiblich","X");
+		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,mutter,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (12,"Teufert","Maria Josefa","Philipp","-","-","-","17.03.1827","Lichtenthal","r&#246m. kath.", "-","Privat","-","-",23,24,"-","20.02.1855","Wien","Maria Josefa Teufert","19.03.1899","Hernals","-","21.03.1899","Friedhof Hernals","-","weiblich","X");
 
 
 		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,mutter,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (13,"Schwab","Heinrich Ludwig I","-","-","-","-","03.11.1839","Nidda","r&#246m. kath. ", "Steindeckermeister","Dachdecker","-","-",25,26,"L&#246wenstein","19.10.1864","Baden","Heinrich Ludwig II, Heuriette, Ludorika, Anna","1905","Baden","-","07.01.1905","Baden","-","m&#228nnlich","X");
@@ -57,7 +50,7 @@
 
 
 
-		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,mutter,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (5,"H&#2520bner","Karl","-","-","-","H&#220-Carl","28.12.1891","Reichenberg (1876 nach Wien &#252bersiedelt)","r&#246m. kath.", "Keine Keine Zeugnisse vorhanden","vielseitig, u.a. Sch&#252tzenmeister im Dorotheum","Errichtung des Familiengrabes am Friedhof Ma. Enzersdorf","Zucht von Kaninchen, Tennis",9,10,"Ullrich","11.11.1884","Wien, Pfarre St. Johann Evang. ","von 8 Kindern &#252berlebt: Carl, Lambert, Rudolf, Leopold","1935","Ma. Enzersdorf","-","-","Friedhof Ma. Enzersdorf","-","m&#228nnlich","X");
+		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,mutter,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (5,"H&#252bner","Karl","-","-","-","H&#220-Carl","28.12.1891","Reichenberg (1876 nach Wien &#252bersiedelt)","r&#246m. kath.", "Keine Keine Zeugnisse vorhanden","vielseitig, u.a. Sch&#252tzenmeister im Dorotheum","Errichtung des Familiengrabes am Friedhof Ma. Enzersdorf","Zucht von Kaninchen, Tennis",9,10,"Ullrich","11.11.1884","Wien, Pfarre St. Johann Evang. ","von 8 Kindern &#252berlebt: Carl, Lambert, Rudolf, Leopold","1935","Ma. Enzersdorf","-","-","Friedhof Ma. Enzersdorf","-","m&#228nnlich","X");
 
 		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,mutter,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (6,"H&#252bner","Maria Josefa","Teufert","-","-","H&#252-Maria","11.01.1857","Hernals","r&#246m. kath.", "-","Gesch&#228ftsf&#252hrerin","-","-",11,12,"Philipp","20.02.1855, 11.11.1884","Hernals, Wien","Carl, Lambert, Rudolf, Leopold","27.05.1927","Maria Enezersdof","Lungenkrankheit","30.05.1927","Maria Enzersdorf","-","weiblich","X");
 
@@ -65,7 +58,7 @@
 
 		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,mutter,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (8,"Schwab","Karoline Johanne","Zinnober","Lini","-","SCHWAB-Karo","20.06.1882","Baden","r&#246m. kath.", "Volkschule, B&#252rgerschule","-","Ab dem Tod des Mannes, F&#252hrung der Dachdeckerei als Witwenbetrieb","-",15,16,"Marian","28.04.1902","Baden","Johanne, Maria","27.05.1958","Baden","Krebs (Gallenblase)","02.06.1958","Baden","-","weiblich","X");
 
-		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,mutter,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (3,"H&#252bner","Leopold","-","Leo","Ingenieur","H&#252-Leo","03.12.1899","Maria Enzernsdorf","r&#246m. kath.", "TGM","Angestellter, ca. 1930 F&#252hrung eines Elektrotechnikunternehmens ","Konzession f&#252r Hoch/Niederspannung","Bergsteigen, Skifahren",5,6,"Schwab","16.03.1932","Baden","Lambert, Heinz, Ludwig, ","16.03.1985","M&#246dling","Altersschw&#228che","29.03.1985","Maria Enzersdorf","Ausbildung zum einj&#228hrigen Freiwilligen, Bef&#246rderung zum Unteroffizier in der Offiziersschule ","m&#228nnlich","X");
+		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,mutter,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (3,"H&#252bner","Leopold","-","Leo","Ingenieur","H&#252-Leo","03.12.1899","Maria Enzernsdorf","r&#246m. kath.", "TGM","Angestellter, ca. 1930 F&#252hrung eines Elektrotechnikunternehmens ","Konzession f&#252r Hoch/Niederspannung","FRAGEN",5,6,"Schwab","16.03.1932","Baden","Lambert, Heinz, Ludwig, ","16.03.1985","M&#246dling","Altersschw&#228che","29.03.1985","Maria Enzersdorf","Ausbildung zum einj&#228hrigen Freiwilligen, Bef&#246rderung zum Unteroffizier in der Offiziersschule ","m&#228nnlich","X");
 
 		INSERT lebensdaten(id,nachname,vorname,maedName,rufname,titel,kurzzeichen,gebDatum,gebOrt,konfession,ausbildung,berufLaufbahn,profTaetigkeiten,hobbies,vater,mutter,gebMutter,trauDatum,trauOrt,kind,verstorbenAm,verstorbenIn,todUrsache,begraebnisAm,begraebnisIn,militaerdienst,geschlecht,stammbaum) VALUES (4,"H&#252bner","Johanna Maria","Schwab","Hausi","nicht bekannt (heute Mittelschulprofessorin)","H&#252-Joh","14.02.1903","Baden","r&#246m. kath.", "Matura (&#214ffentliches M&#228dchenlyzeum Baden), Lehrbefugnispr&#252fung f&#252r Musik an Mittelschulen","Musikp&#228dagogin","-","-",7,8,"Zinober","12.07.1932","Baden","Lambert, Heinz","15.01.1975","Wien","Schilddr&#252senoperation","24.01.1975","-Maria Enzersdorf","-","weiblich","X");
 
@@ -82,6 +75,35 @@
 	UPDATE lebensdaten SET partnerin=2 WHERE id=1;
 	UPDATE lebensdaten SET partnerin=3 WHERE id=4;
 	UPDATE lebensdaten SET partnerin=4 WHERE id=3;
+	UPDATE lebensdaten SET partnerin=5 WHERE id=6;
+	UPDATE lebensdaten SET partnerin=6 WHERE id=5;
+	UPDATE lebensdaten SET partnerin=7 WHERE id=8;
+	UPDATE lebensdaten SET partnerin=8 WHERE id=7;
+	UPDATE lebensdaten SET partnerin=9 WHERE id=10;
+	UPDATE lebensdaten SET partnerin=10 WHERE id=9;
+	UPDATE lebensdaten SET partnerin=11 WHERE id=12;
+	UPDATE lebensdaten SET partnerin=12 WHERE id=11;
+	UPDATE lebensdaten SET partnerin=13 WHERE id=14;
+	UPDATE lebensdaten SET partnerin=14 WHERE id=13;
+	UPDATE lebensdaten SET partnerin=15 WHERE id=16;
+	UPDATE lebensdaten SET partnerin=16 WHERE id=15;
+	UPDATE lebensdaten SET partnerin=17 WHERE id=18;
+	UPDATE lebensdaten SET partnerin=18 WHERE id=17;
+	UPDATE lebensdaten SET partnerin=19 WHERE id=20;
+	UPDATE lebensdaten SET partnerin=20 WHERE id=19;
+	UPDATE lebensdaten SET partnerin=21 WHERE id=22;
+	UPDATE lebensdaten SET partnerin=22 WHERE id=21;
+	UPDATE lebensdaten SET partnerin=23 WHERE id=24;
+	UPDATE lebensdaten SET partnerin=24 WHERE id=23;
+	UPDATE lebensdaten SET partnerin=25 WHERE id=26;
+	UPDATE lebensdaten SET partnerin=26 WHERE id=25;
+	UPDATE lebensdaten SET partnerin=27 WHERE id=28;
+	UPDATE lebensdaten SET partnerin=28 WHERE id=27;
+	UPDATE lebensdaten SET partnerin=29 WHERE id=30;
+	UPDATE lebensdaten SET partnerin=30 WHERE id=29;
+	UPDATE lebensdaten SET partnerin=31 WHERE id=32;
+	UPDATE lebensdaten SET partnerin=32 WHERE id=31;
+
 
 
 
--- nahansicht.php
+++ nahansicht.php
@@ -10,20 +10,20 @@ $person=$row->fetch();
 //Query die den Namen des Vaters zurückgibt
 $query=$pdo -> query("SELECT vorname,nachname FROM lebensdaten WHERE id=$person[vater]");
 $vater_row = $query->fetch(PDO::FETCH_ASSOC);
-$vater = $vater_row['vorname'] . " " . strtoupper($vater_row['nachname']);
+$vater = $vater_row['vorname'] . " " . $vater_row['nachname'];
 
 //Query die den Namen der Mutter zurückgibt
 $query=$pdo -> query("SELECT vorname,nachname FROM lebensdaten WHERE id=$person[mutter]");
 $mutter_row = $query->fetch(PDO::FETCH_ASSOC);
-$mutter = $mutter_row['vorname'] . " " . strtoupper($mutter_row['nachname']);
+$mutter = $mutter_row['vorname'] . " " . $mutter_row['nachname'];
 
 //Query die den Namen des Partners zurückgibt
 $query=$pdo -> query("SELECT vorname,nachname FROM lebensdaten WHERE id=$person[partnerin]");
 $partnerin_row = $query->fetch(PDO::FETCH_ASSOC);
-$partnerin = $partnerin_row['vorname'] . " " . strtoupper($partnerin_row['nachname']);
+$partnerin = $partnerin_row['vorname'] . " " . $partnerin_row['nachname'];
 
 
-//Geburtsdatum Format anpassen
+/**Geburtsdatum Format anpassen
 if(isset($person['gebDatum']))	{
 	try {
 		$person['gebDatum']=substr($person['gebDatum'],8,2) . "." . substr($person['gebDatum'],5,2) . "." . substr($person['gebDatum'],0,4);
@@ -31,7 +31,7 @@ if(isset($person['gebDatum']))	{
 	catch(Exception $e)	{
 		$meldung.="Geburtsdatumsumwandlung hat nicht funktioniert";
 	}
-}
+}*/
 
 //Alle Null werte ersetzen
 /**
